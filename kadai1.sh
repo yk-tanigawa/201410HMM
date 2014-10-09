79ef8f9e64637caa4064077bd9d6e372ff1bdaa5
Yosuke Tanigawa <yosuke@Yosuke-MBP.local>
2014-10-09 23:30:54 +0900

    課題2: 前向きアルゴリズムと後ろ向きアルゴリズムの実装まで完了

--- kadai1.sh
+++
@@ -1,9 +0,0 @@
-make clean
-echo "+---------- 課題1 Viterbi アルゴリズム ----------+"
-echo " * buildして実行します。しばらくお待ちください..."
-make hmm
-./hmm params.txt sample-RNA.fa > "kadai1_out.txt"
-make clean
-echo " * 実行結果は次の通りです。"
-cat kadai1_out.txt
-echo " * 結果は kadai1_out.txt にも出力されました。"
